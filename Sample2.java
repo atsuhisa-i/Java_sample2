55531b6688abceb31fb6857c0369e9e59d00e7df
atsuhisa-i <atsuhisa.1124@gmail.com>
2021-02-13 23:11:48 +0900

    GUIの基本2

--- Sample2.java
+++ Sample2.java
@@ -6,16 +6,26 @@ import javafx.scene.layout.*;
 
 public class Sample2 extends Application
 {
+  private Button bt;
+
   public static void main(String args[])
   {
     launch(args);
   }
   public void start(Stage stage)throws Exception
   {
+    bt = new Button();
+
+    bt.setText("購入");
+
     BorderPane bp = new BorderPane();
+
+    bp.setCenter(bt);
+
     Scene sc = new Scene(bp, 300, 200);
 
     stage.setScene(sc);
+    
     stage.setTitle("サンプル");
     stage.show();
   }
