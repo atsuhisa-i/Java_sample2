b7c585e41ac22ab89cc88b73308670282ef7f9e1
atsuhisa-i <atsuhisa.1124@gmail.com>
2021-02-16 21:57:49 +0900

    コントロールの応用5

--- Sample5.java
+++ Sample5.java
@@ -3,12 +3,12 @@ import javafx.stage.*;
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
-import javafx.scene.input.*;
-import javafx.event.*;
+import javafx.scene.paint.*;
+import javafx.scene.text.*;
 
 public class Sample5 extends Application
 {
-  private Label lb;
+  private Label[] lb = new Label[3];
 
   public static void main(String[] args)
   {
@@ -16,24 +16,27 @@ public class Sample5 extends Application
   }
   public void start(Stage stage)throws Exception
   {
-    lb = new Label("いらっしゃいませ。");
+    for(int i=0; i<lb.length; i++){
+      lb[i] = new Label("車" + i + "はいかがですか？");
+    }
+
+    lb[0].setTextFill(Color.BLACK);
+    lb[1].setTextFill(Color.BLUE);
+    lb[2].setTextFill(Color.RED);
 
     BorderPane bp = new BorderPane();
+    VBox vb = new VBox();
 
-    bp.setTop(lb);
+    for(int i=0; i<lb.length; i++){
+      vb.getChildren().add(lb[i]);
+    }
+    bp.setCenter(vb);
 
     Scene sc = new Scene(bp, 300, 200);
 
-    sc.addEventHandler(MouseEvent.MOUSE_CLICKED, new SampleEventHandler());
-
     stage.setScene(sc);
+
+    stage.setTitle("サンプル");
     stage.show();
-  }
-  class SampleEventHandler implements EventHandler<MouseEvent>
-  {
-    public void handle(MouseEvent e)
-    {
-      lb.setText("ご購入ありがとうございます。");
-    }
   }
 }
