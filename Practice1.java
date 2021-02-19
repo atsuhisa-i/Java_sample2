0d8ca569bfc2ab5b5a79db131032d9fd6fff8a60
atsuhisa-i <atsuhisa.1124@gmail.com>
2021-02-19 22:04:21 +0900

    コントロールの応用12

--- Practice1.java
+++ Practice1.java
@@ -3,12 +3,16 @@ import javafx.stage.*;
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
+import javafx.scene.paint.*;
+import javafx.scene.input.*;
 import javafx.event.*;
+import javafx.geometry.*;
 
 public class Practice1 extends Application
 {
   private Label lb;
-  private Button bt;
+  private RadioButton rb1, rb2, rb3;
+  private ToggleGroup tg;
 
   public static void main(String[] args)
   {
@@ -17,14 +21,32 @@ public class Practice1 extends Application
   public void start(Stage stage)throws Exception
   {
     lb = new Label("いらっしゃいませ。");
-    bt = new Button("購入");
+    rb1 = new RadioButton("黄");
+    rb2 = new RadioButton("赤");
+    rb3 = new RadioButton("青");
+    tg = new ToggleGroup();
+
+    rb1.setToggleGroup(tg);
+    rb2.setToggleGroup(tg);
+    rb3.setToggleGroup(tg);
+
+    rb1.setSelected(true);
+    lb.setBackground(new Background(new BackgroundFill(Color.YELLOW, null, null)));
 
     BorderPane bp = new BorderPane();
+    HBox hb = new HBox();
+
+    hb.getChildren().add(rb1);
+    hb.getChildren().add(rb2);
+    hb.getChildren().add(rb3);
+    hb.setAlignment(Pos.CENTER);
 
     bp.setTop(lb);
-    bp.setCenter(bt);
+    bp.setCenter(hb);
 
-    bt.setOnAction(new SampleEventHandler());
+    rb1.setOnAction(new SampleEventHandler());
+    rb2.setOnAction(new SampleEventHandler());
+    rb3.setOnAction(new SampleEventHandler());
 
     Scene sc = new Scene(bp, 300, 200);
 
@@ -37,7 +59,15 @@ public class Practice1 extends Application
   {
     public void handle(ActionEvent e)
     {
-      bt.setText("Thanks!");
+      RadioButton tmp = (RadioButton) e.getSource();
+      if(tmp == rb1)
+        lb.setBackground(new Background(new BackgroundFill(Color.YELLOW, null, null)));
+      
+      else if(tmp == rb2)
+        lb.setBackground(new Background(new BackgroundFill(Color.RED, null, null)));
+      
+      else if(tmp == rb3)
+        lb.setBackground(new Background(new BackgroundFill(Color.BLUE, null, null)));
     }
   }
 }
