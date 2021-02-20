854664fc398f79c572578b81aa8eba905099c955
atsuhisa-i <atsuhisa.1124@gmail.com>
2021-02-20 13:07:38 +0900

    コントロールの応用14

--- Practice3.java
+++ Practice3.java
@@ -4,11 +4,15 @@ import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.input.*;
+import javafx.scene.text.*;
 import javafx.event.*;
+import javafx.geometry.*;
 
 public class Practice3 extends Application
 {
-  private Label lb1, lb2;
+  private Label lb;
+  private RadioButton rb1, rb2, rb3;
+  private ToggleGroup tg;
 
   public static void main(String[] args)
   {
@@ -16,31 +20,53 @@ public class Practice3 extends Application
   }
   public void start(Stage stage)throws Exception
   {
-    lb1 = new Label("キーを押して下さい。");
-    lb2 = new Label();
+    lb = new Label("Hello!");
+    rb1 = new RadioButton("普通");
+    rb2 = new RadioButton("太字");
+    rb3 = new RadioButton("イタリック");
+    tg = new ToggleGroup();
+
+    rb1.setToggleGroup(tg);
+    rb2.setToggleGroup(tg);
+    rb3.setToggleGroup(tg);
+
+    rb1.setSelected(true);
 
     BorderPane bp = new BorderPane();
+    HBox hb = new HBox();
 
-    bp.setTop(lb1);
-    bp.setBottom(lb2);
+    hb.getChildren().add(rb1);
+    hb.getChildren().add(rb2);
+    hb.getChildren().add(rb3);
+    hb.setAlignment(Pos.CENTER);
+
+    bp.setTop(lb);
+    bp.setCenter(hb);
+
+    rb1.setOnAction(new SampleEventHandler());
+    rb2.setOnAction(new SampleEventHandler());
+    rb3.setOnAction(new SampleEventHandler());
 
     Scene sc = new Scene(bp, 300, 200);
-
-    sc.setOnKeyPressed(new SampleEventHandler());
 
     stage.setScene(sc);
 
     stage.setTitle("サンプル");
     stage.show();
   }
-  class SampleEventHandler implements EventHandler<KeyEvent>
+  class SampleEventHandler implements EventHandler<ActionEvent>
   {
-    public void handle(KeyEvent e)
+    public void handle(ActionEvent e)
     {
-      String str;
-      KeyCode k = e.getCode();
-      str = k.toString();
-      lb2.setText(str + "ですね。");
+      RadioButton tmp = (RadioButton) e.getSource();
+      if(tmp == rb1)
+        lb.setFont(Font.font("Serif", 12));
+      
+      else if(tmp == rb2)
+        lb.setFont(Font.font("Serif", FontWeight.BOLD, 12));
+      
+      else if(tmp == rb3)
+        lb.setFont(Font.font("Serif", FontPosture.ITALIC, 12));
     }
   }
 }
