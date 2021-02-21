e2256d1cc0e82a7e3ebe135d99ef778ef4fa1c3a
atsuhisa-i <atsuhisa.1124@gmail.com>
2021-02-21 17:01:18 +0900

    コントロールの活用10

--- Practice1.java
+++ Practice1.java
@@ -3,16 +3,17 @@ import javafx.stage.*;
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
-import javafx.scene.paint.*;
 import javafx.scene.input.*;
 import javafx.event.*;
-import javafx.geometry.*;
+import javafx.collections.*;
+import javafx.beans.value.*;
+import java.time.*;
+import java.time.format.*;
 
 public class Practice1 extends Application
 {
   private Label lb;
-  private RadioButton rb1, rb2, rb3;
-  private ToggleGroup tg;
+  private ListView<String> lv;
 
   public static void main(String[] args)
   {
@@ -21,32 +22,26 @@ public class Practice1 extends Application
   public void start(Stage stage)throws Exception
   {
     lb = new Label("いらっしゃいませ。");
-    rb1 = new RadioButton("黄");
-    rb2 = new RadioButton("赤");
-    rb3 = new RadioButton("青");
-    tg = new ToggleGroup();
+    lv = new ListView<String>();
 
-    rb1.setToggleGroup(tg);
-    rb2.setToggleGroup(tg);
-    rb3.setToggleGroup(tg);
+    ObservableList<String> ol =
+      FXCollections.observableArrayList();
+    
+    DateTimeFormatter df = 
+      DateTimeFormatter.ofPattern("yyyy/MM/dd");
+    LocalDateTime t = LocalDateTime.now();
 
-    rb1.setSelected(true);
-    lb.setBackground(new Background(new BackgroundFill(Color.YELLOW, null, null)));
+    for(int i=0; i<50; i++){
+      ol.add(df.format(t.plusDays(i)));
+    }
+    lv.setItems(ol);
 
     BorderPane bp = new BorderPane();
-    HBox hb = new HBox();
-
-    hb.getChildren().add(rb1);
-    hb.getChildren().add(rb2);
-    hb.getChildren().add(rb3);
-    hb.setAlignment(Pos.CENTER);
 
     bp.setTop(lb);
-    bp.setCenter(hb);
+    bp.setCenter(lv);
 
-    rb1.setOnAction(new SampleEventHandler());
-    rb2.setOnAction(new SampleEventHandler());
-    rb3.setOnAction(new SampleEventHandler());
+    lv.getSelectionModel().selectedItemProperty().addListener(new SampleChangeListener());
 
     Scene sc = new Scene(bp, 300, 200);
 
@@ -55,19 +50,12 @@ public class Practice1 extends Application
     stage.setTitle("サンプル");
     stage.show();
   }
-  class SampleEventHandler implements EventHandler<ActionEvent>
+
+  class SampleChangeListener implements ChangeListener<String>
   {
-    public void handle(ActionEvent e)
+    public void changed(ObservableValue ob, String bs, String as)
     {
-      RadioButton tmp = (RadioButton) e.getSource();
-      if(tmp == rb1)
-        lb.setBackground(new Background(new BackgroundFill(Color.YELLOW, null, null)));
-      
-      else if(tmp == rb2)
-        lb.setBackground(new Background(new BackgroundFill(Color.RED, null, null)));
-      
-      else if(tmp == rb3)
-        lb.setBackground(new Background(new BackgroundFill(Color.BLUE, null, null)));
+      lb.setText(as + "ですね。");
     }
   }
 }
