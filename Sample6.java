fca6e2e171ebe7a99959fc6e99597f971515bcdc
atsuhisa-i <atsuhisa.1124@gmail.com>
2021-02-24 22:54:17 +0900

    サーブレット6

--- Sample6.java
+++ Sample6.java
@@ -1,71 +1,25 @@
-import javafx.application.*;
-import javafx.stage.*;
-import javafx.scene.*;
-import javafx.scene.control.*;
-import javafx.scene.layout.*;
-import javafx.scene.input.*;
-import javafx.event.*;
-import javafx.scene.image.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
-public class Sample6 extends Application
+public class Sample6 extends HttpServlet
 {
-  private Label lb;
-  private Button[] bt = new Button[3];
-  private ToolBar tb;
-  private Image im;
-
-  public static void main(String[] args)
+  public void doGet(HttpServletRequest request,
+    HttpServletResponse response)throws ServletException
   {
-    launch(args);
-  }
-  public void start(Stage stage)throws Exception
-  {
-    lb = new Label("いらっしゃいませ。");
-    tb = new ToolBar();
-    im = new Image(getClass().getResourceAsStream("car.jpg"));
+    try{
+      String carname = request.getParameter("cars");
 
-    for(int i=0; i<bt.length; i++){
-      bt[i] = new Button();
-      bt[i].setGraphic(new ImageView(im));
+      ServletContext sc = getServletContext();
+
+      if(carname.length() !=0){
+        sc.getRequestDispatcher("/servlet/Sample2").forward(request, response);
+      }
+      else{
+        sc.getRequestDispatcher("/error.html").forward(request, response);
+      }
     }
-
-    for(int i=0; i<bt.length; i++){
-      bt[i].setTooltip(new Tooltip((i+1) + "号車"));
-    }
-
-    tb.getItems().addAll(bt[0], bt[1], new Separator(), bt[2]);
-
-    BorderPane bp = new BorderPane();
-
-    bp.setTop(tb);
-    bp.setCenter(lb);
-
-    for(int i=0; i<bt.length; i++){
-      bt[i].setOnAction(new SampleEventHandler());
-    }
-
-    Scene sc = new Scene(bp, 300, 200);
-
-    stage.setScene(sc);
-
-    stage.setTitle("サンプル");
-    stage.show();
-  }
-
-  class SampleEventHandler implements EventHandler<ActionEvent>
-  {
-    public void handle(ActionEvent e)
-    {
-      int num = 0;
-      Button tmp = (Button) e.getSource();
-
-      if(tmp == bt[0])
-        num = 1;
-      else if(tmp == bt[1])
-        num = 2;
-      else if(tmp == bt[2])
-        num = 3;
-      lb.setText(num + "号車ですね。");
+    catch(Exception e){
+      e.printStackTrace();
     }
   }
 }
