a9ece7bee9970f8ecae85748513fc0db6d5c21c1
atsuhisa-i <atsuhisa.1124@gmail.com>
2021-03-09 20:14:25 +0900

    JDBC3

--- Sample3.java
+++ Sample3.java
@@ -1,38 +1,43 @@
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
+import java.sql.*;
 
-public class Sample3 extends HttpServlet
+public class Sample3
 {
-  public void doGet(HttpServletRequest request,
-    HttpServletResponse response)throws ServletException
+  public static void main(String args[])
   {
+    if(args.length != 2){
+      System.out.println("パラメータの数が違います。");
+      System.exit(1);
+    }
+    
     try{
-      String carname = request.getParameter("cars");
+      String url = "jdbc:derby:cardb;create=true";
+      String usr = "";
+      String pw = "";
 
-      response.setContentType("text/html; charset=UTF-8");
+      Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
 
-      PrintWriter pw = response.getWriter();
-      if(carname.length() !=0){
-        pw.println("<!DOCTYPE html><html>\n"
-        + "<head><title>"
-        + carname + "</title></head>\n"
-        + "<body><div style=\"text-align: center;\">\n"
-        + "<h2>\n" + carname + "</h2>\n"
-        + carname
-        + "のお買い上げありがとうございました。<br/>\n"
-        + "</div></body>\n"
-        + "</html>\n");
+      Connection cn = DriverManager.getConnection(url, usr, pw);
+
+      Statement st = cn.createStatement();
+      String qry1 = "INSERT INTO 車表 VALUES (" + args[0] + " , '" + args[1] + "')";
+      String qry2 = "SELECT * FROM 車表";
+
+      st.executeUpdate(qry1);
+      ResultSet rs = st.executeQuery(qry2);
+
+      ResultSetMetaData rm = rs.getMetaData();
+      int cnum = rm.getColumnCount();
+      while(rs.next()){
+        for(int i=1; i<=cnum; i++){
+          System.out.print(rm.getColumnName(i) + ":" + 
+            rs.getObject(i) + "  ");
+        }
+        System.out.println("");
       }
-      else{
-        pw.println("<!DOCTYPE html><html>\n"
-        + "<head><title>エラー</title></head>\n"
-        + "<body><div style=\"text-align: center;\">\n"
-        + "<h2>エラー</h2>\n"
-        + "入力して下さい。<br/>\n"
-        + "</div></body>\n"
-        + "</html>\n");
-      }
+
+      rs.close();
+      st.close();
+      cn.close();
     }
     catch(Exception e){
       e.printStackTrace();
