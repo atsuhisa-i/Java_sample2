3aca36257330aab0b9115d2e327a85e08ddbe898
atsuhisa-i <atsuhisa.1124@gmail.com>
2021-03-11 06:28:39 +0900

    JDBC5

--- Practice1.java
+++ Practice1.java
@@ -1,35 +1,60 @@
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
+import java.sql.*;
 
-public class Practice1 extends HttpServlet
+public class Practice1
 {
-  public void doGet(HttpServletRequest request,
-    HttpServletResponse response)throws ServletException
+  public static void main(String[] args)
   {
     try{
-      String personname = request.getParameter("person");
+      String url = "jdbc:derby:fooddb;create=true";
+      String usr = "";
+      String pw = "";
 
-      ServletContext sc = getServletContext();
+      Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
 
-      response.setContentType("text/html; charset=UTF-8");
+      Connection cn = DriverManager.getConnection(url, usr,pw);
 
-      if(personname.length() !=0){
-        PrintWriter pw = response.getWriter();
-        pw.println("<!DOCTYPE html><html>\n"
-        + "<head><title>\n" + personname 
-        + "</title></head>\n"
-        + "<body><div style=\"text-align: center;\">\n"
-        + "<h2>\nようこそ"
-        + "</h2>\n" + personname
-        + "さん、いらっしゃいませ。<br/>\n"
-        + "</div></body>\n"
-        + "</html>\n");
+      DatabaseMetaData dm = cn.getMetaData();
+      ResultSet tb = dm.getTables(null, null, "果物表", null);
+
+      Statement st = cn.createStatement();
+
+      String qry1 = "CREATE TABLE 果物表(番号 int, 名前 varchar(50), 取扱店 varchar(50))";
+
+      String[] qry2 = {
+        "INSERT INTO 果物表 VALUES (1, 'みかん', '青山商店')",
+        "INSERT INTO 果物表 VALUES (2, 'りんご', '東京市場')",
+        "INSERT INTO 果物表 VALUES (3, 'バナナ', '鈴木貨物')",
+        "INSERT INTO 果物表 VALUES (4, 'イチゴ', '東京市場')",
+        "INSERT INTO 果物表 VALUES (5, 'なし', '青山商店')",
+        "INSERT INTO 果物表 VALUES (6, '栗', '横浜デパート')",
+        "INSERT INTO 果物表 VALUES (7, 'モモ', '横浜デパート')",
+        "INSERT INTO 果物表 VALUES (8, 'びわ', '佐藤商店')",
+        "INSERT INTO 果物表 VALUES (9, '柿', '青山市場')",
+        "INSERT INTO 果物表 VALUES (10, 'スイカ', '東京市場')"};
+      String qry3 = "SELECT * FROM 果物表";
+
+      if(!tb.next()){
+        st.executeUpdate(qry1);
+        for(int i=0; i<qry2.length; i++){
+          st.executeUpdate(qry2[i]);
+        }
       }
-      else{
-        sc.getRequestDispatcher("/error.html")
-          .forward(request, response);
+
+      ResultSet rs = st.executeQuery(qry3);
+
+      ResultSetMetaData rm = rs.getMetaData();
+      int cnum = rm.getColumnCount();
+      while(rs.next()){
+        for(int i=1; i<=cnum; i++){
+          System.out.print(rm.getColumnName(i) + ":" + 
+            rs.getObject(i) + "  ");
+        }
+        System.out.println("");
       }
+
+      rs.close();
+      st.close();
+      cn.close();
     }
     catch(Exception e){
       e.printStackTrace();
