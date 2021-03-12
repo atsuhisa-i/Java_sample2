740fd0007fb3e0dd181522fd5a7c3c7b1749879f
atsuhisa-i <atsuhisa.1124@gmail.com>
2021-03-12 19:21:16 +0900

    file-operation9

--- Sample9.java
+++ Sample9.java
@@ -1,14 +1,20 @@
+import java.io.*;
+import java.util.regex.*;
 import javafx.application.*;
 import javafx.stage.*;
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
-import javafx.scene.paint.*;
-import javafx.scene.canvas.*;
+import javafx.scene.input.*;
+import javafx.event.*;
 
 public class Sample9 extends Application
 {
-  private Canvas cv;
+  private Label lb;
+  private TextArea ta;
+  private TextField tf;
+  private Button bt1, bt2;
+  private Matcher mt;
 
   public static void main(String[] args)
   {
@@ -16,25 +22,25 @@ public class Sample9 extends Application
   }
   public void start(Stage stage)throws Exception
   {
-    cv = new Canvas(300, 200);
-
-    GraphicsContext gc = cv.getGraphicsContext2D();
-
-    for(int i=0; i<100; i++){
-      int r = (int) (Math.random() * 256);
-      int g = (int) (Math.random() * 256);
-      int b = (int) (Math.random() * 256);
-
-      double x = Math.random() * 300;
-      double y = Math.random() * 200;
-
-      gc.setFill(Color.rgb(r, g, b, 1.0));
-      gc.fillOval(x, y, 10, 10);
-    }
+    lb = new Label("入力して下さい。");
+    ta = new TextArea();
+    tf = new TextField();
+    bt1 = new Button("検索");
+    bt2 = new Button("次へ");
 
     BorderPane bp = new BorderPane();
+    HBox hb = new HBox();
 
-    bp.setCenter(cv);
+    hb.getChildren().add(tf);
+    hb.getChildren().add(bt1);
+    hb.getChildren().add(bt2);
+
+    bp.setTop(lb);
+    bp.setCenter(ta);
+    bp.setBottom(hb);
+
+    bt1.setOnAction(new SampleEventHandler());
+    bt2.setOnAction(new SampleEventHandler());
 
     Scene sc = new Scene(bp, 300, 200);
 
@@ -42,5 +48,32 @@ public class Sample9 extends Application
 
     stage.setTitle("サンプル");
     stage.show();
+  }
+
+  class SampleEventHandler implements EventHandler<ActionEvent>
+  {
+    public void handle(ActionEvent e)
+    {
+      try{
+        if(e.getSource() == bt1){
+          Pattern pn = Pattern.compile(tf.getText());
+          mt = pn.matcher(ta.getText());
+          if(mt.find() != false){
+            ta.selectRange(mt.start(), mt.end());
+          }
+          else{
+            ta.home();
+          }
+        }
+        else if(e.getSource() == bt2){
+          if(mt.find() != false && mt.pattern().pattern().equals(tf.getText())){
+            ta.selectRange(mt.start(), mt.end());
+          }
+        }
+      }
+      catch(Exception ex){
+        ex.printStackTrace();
+      }
+    }
   }
 }
