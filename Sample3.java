3f3059254290097df1167fa41076b81dc403da86
atsuhisa-i <atsuhisa.1124@gmail.com>
2021-03-14 17:23:41 +0900

    network3

--- Sample3.java
+++ Sample3.java
@@ -1,51 +1,74 @@
-import java.io.*;
-import java.util.*;
-import javax.xml.parsers.*;
-import javax.xml.transform.*;
-import javax.xml.transform.stream.*;
-import javax.xml.transform.dom.*;
-import org.w3c.dom.*;
+import java.net.*;
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.input.*;
+import javafx.event.*;
 
-public class Sample3
+public class Sample3 extends Application
 {
-  public static void main(String[] args)throws Exception
+  private Label lb1, lb2, lb3;
+  private TextField tf1, tf2, tf3;
+  private Button bt;
+
+  public static void main(String[] args)
   {
-    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-    DocumentBuilder db = dbf.newDocumentBuilder();
+    launch(args);
+  }
+  public void start(Stage stage)throws Exception
+  {
+    try{
+      InetAddress ia = InetAddress.getLocalHost();
 
-    Document doc = db.newDocument();
+      lb1 = new Label("入力して下さい。");
+      lb2 = new Label("ホスト名");
+      lb3 = new Label("IPアドレス");
+      tf1 = new TextField();
+      tf2 = new TextField();
+      tf3 = new TextField();
+      bt = new Button("検索");
 
-    Element root = doc.createElement("車リスト");
-    doc.appendChild(root);
+      BorderPane bp = new BorderPane();
+      VBox vb = new VBox();
 
-    BufferedReader br = new BufferedReader(new FileReader("Sample.csv"));
+      vb.getChildren().add(lb1);
+      vb.getChildren().add(tf1);
+      vb.getChildren().add(lb2);
+      vb.getChildren().add(tf2);
+      vb.getChildren().add(lb3);
+      vb.getChildren().add(tf3);
 
-    ArrayList<String> colname = new ArrayList<String>();
-    String line = br.readLine();
-    StringTokenizer stt = new StringTokenizer(line, ",");
-    while(stt.hasMoreTokens()){
-      colname.add(stt.nextToken());
+      bp.setCenter(vb);
+      bp.setBottom(bt);
+
+      bt.setOnAction(new SampleEventHandler());
+
+      Scene sc = new Scene(bp, 300, 200);
+
+      stage.setScene(sc);
+
+      stage.setTitle("サンプル");
+      stage.show();
     }
+    catch(Exception e){
+      e.printStackTrace();
+    }
+  }
 
-    while((line = br.readLine()) != null){
-      StringTokenizer std = new StringTokenizer(line, ",");
-      Element car = doc.createElement("車");
-      root.appendChild(car);
-
-      for(int i=0; i<colname.size(); i++){
-        Element elm = doc.createElement((String)colname.get(i));
-        Text txt = doc.createTextNode(std.nextToken());
-        elm.appendChild(txt);
-        car.appendChild(elm);
+  class SampleEventHandler implements EventHandler<ActionEvent>
+  {
+    public void handle(ActionEvent e)
+    {
+      try{
+        InetAddress ia = InetAddress.getByName(tf1.getText());
+        tf2.setText(ia.getHostName());
+        tf3.setText(ia.getHostAddress());
+      }
+      catch(Exception ex){
+        ex.printStackTrace();
       }
     }
-    br.close();
-
-    TransformerFactory tff = TransformerFactory.newInstance();
-    Transformer tf = tff.newTransformer();
-    tf.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-    tf.transform(new DOMSource(doc),
-      new StreamResult("result.xml"));
-    System.out.println("result.xmlに出力しました。");
   }
 }
