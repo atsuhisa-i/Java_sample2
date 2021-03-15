8f3a3e07e141f2a1c5783e95f66e7383a780b691
atsuhisa-i <atsuhisa.1124@gmail.com>
2021-03-15 11:14:37 +0900

    network7

--- Practice1C.java
+++ Practice1C.java
@@ -10,8 +10,11 @@ import javafx.event.*;
 
 public class Practice1C extends Application
 {
-  public static final String HOST = "localhost";
-  public static final int PORT = 10000;
+  // public static final String HOST = "localhost";
+  // public static final int PORT = 10000;
+
+  private Label lb1, lb2;
+  private TextField tf1, tf2;
 
   private TextArea ta;
   private Button bt;
@@ -25,11 +28,22 @@ public class Practice1C extends Application
     try{
       InetAddress ia = InetAddress.getLocalHost();
 
+      lb1 = new Label("ホスト");
+      lb2 = new Label("ポート");
+      tf1 = new TextField();
+      tf2 = new TextField();
       ta = new TextArea();
       bt = new Button("接続");
 
+      GridPane gp = new GridPane();
       BorderPane bp = new BorderPane();
 
+      gp.add(lb1, 0, 0);
+      gp.add(lb2, 0, 1);
+      gp.add(tf1, 1, 0);
+      gp.add(tf2, 1, 1);
+
+      bp.setTop(gp);
       bp.setCenter(ta);
       bp.setBottom(bt);
 
@@ -51,7 +65,11 @@ public class Practice1C extends Application
     public void handle(ActionEvent e)
     {
       try{
-        Socket sc = new Socket(HOST, PORT);
+        InetAddress ia = InetAddress.getByName(tf1.getText());
+        String host = ia.getHostName();
+        int port = Integer.parseInt(tf2.getText());
+
+        Socket sc = new Socket(host, port);
         BufferedReader br = new BufferedReader(new InputStreamReader(sc.getInputStream()));
         String str = br.readLine();
         ta.setText(str);
